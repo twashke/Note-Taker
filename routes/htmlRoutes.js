fce41d66ab85009129f134cee1e68ef2eb93864b
Tiffany Washke <twashke@gmail.com>
2021-06-20 17:56:32 -0700

    updated html routes, working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,20 @@
 // Declare dependcies
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = app => {
+// -----------------------------------------|
+//          HTML Route Requests             | 
+// -----------------------------------------|
 
-    // Get request for notes page
-    app.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+// Get request for notes page
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/notes.html"));
+});
+    
+// Get request default for home page
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/index.html"));
+});
 
-    // Get request default for home page
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-};
-
+module.exports = router;
 
