ae7d395e806eb86561e685a8f73bb59ff7e44e3e
Tiffany Washke <twashke@gmail.com>
2021-06-20 17:57:00 -0700

    updated server.js file, nopw working

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-// Declaring Dependencies
+// Declare Dependencies
 const express = require("express");
-// Creating an express server 
+// Declare Variable for Routes
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 const app = express();
-const path = require("path");
-// Creating a port for the server
-const PORT = process.env.PORT || 8080;
+
+// Creating PORT for Server
+const PORT = process.env.PORT || 3001;
 
 // Link to public folder
 app.use(express.static("public"));
 
-// Maps server to router files
-require("./routes/htmlRoutes")(app);
-require("./routes/apiRoutes")(app);
-// Link to public folder
-app.use(express.static(__dirname));
 // Data parsing using the express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Map to API route
+app.use("/api", apiRoutes);
+// Map to HTML route
+app.use("/", htmlRoutes);
 
-// Listener to start the server
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+    console.log(`App listening on PORT: ${PORT}`)
 });
