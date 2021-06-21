543c9af830da0be4005b189d44dbb58decb0f109
Tiffany Washke <twashke@gmail.com>
2021-06-20 17:56:00 -0700

    updated apiRoutes, working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,57 +1,66 @@
 // Declare dependcies
+const router = require("express").Router();
 const fs = require("fs");
 // Add a unique ID
 const { v4: uuidv4 } = require("uuid");
+// Acces db.json file
+const dbFile = require("../db/db.json");
 
-module.exports = app => {
-    // Read file, throw note if issue occurs
-    fs.readFile("db/db.json", (err, data) => {
-    // Throw error if issue occurs
-    if (err) throw err;
-    // Declare variable for notes
-    let notes = JSON.parse(data);
-    console.log(notes);
+// -----------------------------------------|
+//               Functions                  | 
+// -----------------------------------------|
 
-    // GET Request
-    app.get("/api/notes", (req, res) => {
-        // Read file and return response
-        res.json(notes);
+// Function to Save New Notes
+function dbUpdate() {
+    // Read file
+    fs.writeFile("db/db.json", JSON.stringify(dbFile, "\t"), err => {
+        // Throw error if issue occurs
+        if (err) throw err;
+        return true;
     });
-    
-    // POST Request
-    app.post("/api/notes", (req, res) => {
-        // Declare Variable for new note
-        let newNote = req.body;
-        // Create a unique id
-        newNote.id = uuidv4();
-        // Push new notes to notes
-        notes.push(newNote);
-        dbUpdate();
-        // Console log the notes were entered
-        console.log("Added new note to JSON file");
-    });
-
-    // GET Request for a specific id
-    app.get("/api/notes/:id", (req, res) => {
-        res.json(notes[req.params.id]);
-    })
-
-    // DELETE Request for a specific id
-    app.delete("/api/notes/:id", (req, res) => {
-        notes.splice(req.params.id, 1);
-        dbUpdate();
-        console.log("Note Deleted");
-    })
-        
-    function dbUpdate() {
-        fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), err => {
-            if (err) throw err;
-            return true;
-        });
-    }
-});
 };
 
+// -----------------------------------------|
+//            API Route Requests            | 
+// -----------------------------------------|
+    
+// GET Request
+router.get("/notes", (req, res) => {
+// Read file and return response
+    res.json(dbFile);
+    console.log("Request successfull");
+});
 
+// POST Request
+router.post("/notes", (req, res) => {
+    // Declare Variables for new note
+    const title = req.body.title;
+    const text = req.body.text;
+    const id = uuidv4();
+    // Declare newNote fields
+    const newNote = {
+        title: title,
+        text: text,
+        id: id
+    }
+    // Push new notes to notes
+    dbFile.push(newNote);
+    dbUpdate();
+    res.json(dbFile);
+    // Console log the notes were entered
+    console.log("Added new note to JSON file");
+});
 
+// GET Request for a specific id
+router.get("/notes/:id", (req, res) => {
+    res.json(dbFile[req.params.id]);
+})
 
+// DELETE Request for a specific id
+router.delete("/notes/:id", (req, res) => {
+    dbFile.splice(req.params.id, 1);
+    dbUpdate();
+    res.json(dbFile);
+})
+
+module.exports = router;
